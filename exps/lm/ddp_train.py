7d500ce2a72d05a52182eb47aa783ea5d3bbed9c
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2021-12-20 15:53:35 +0700

    Create ddp_train.py

---
+++ exps/lm/ddp_train.py
@@ -0,0 +1,96 @@
+import copy
+import time
+import torch
+import torch.nn as nn
+import math
+from ddp_opt import DDPNOPT
+from ddp_models import TransformerModel
+from _data import get_data, get_batch, generate_square_subsequent_mask
+
+train_data, val_data, test_data, vocab = get_data()
+
+device = 'cuda'
+ntokens = len(vocab)  # size of vocabulary
+emsize = 100  # embedding dimension
+d_hid = 100  # dimension of the feedforward network model in nn.TransformerEncoder
+nlayers = 10  # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhead = 2  # number of heads in nn.MultiheadAttention
+dropout = 0.2  # dropout probability
+bptt = 35
+model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
+
+criterion = nn.CrossEntropyLoss()
+lr = 1e-4 # learning rate
+# optimizer = torch.optim.RMSprop(model.parameters(), lr=lr)
+optimizer = DDPNOPT(model, lr=lr, lrddp=1.0)
+
+def train(model, train_data, bptt, criterion, ntokens, device):
+    model.train()  # turn on train mode
+    total_loss = 0.
+    log_interval = 300
+    start_time = time.time()
+    src_mask = generate_square_subsequent_mask(bptt).to(device)
+
+    num_batches = len(train_data) // bptt
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i)
+        batch_size = data.size(0)
+        if batch_size != bptt:  # only on last batch
+            src_mask = src_mask[:batch_size, :batch_size]
+        output = model(data, src_mask)
+        loss = criterion(output.view(-1, ntokens), targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step(data, src_mask)
+
+        total_loss += loss.item()
+        if batch % log_interval == 0 and batch > 0:
+            # lr = scheduler.get_last_lr()[0]
+            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            cur_loss = total_loss / log_interval
+            ppl = math.exp(cur_loss)
+            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                  f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+                  f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            total_loss = 0
+            start_time = time.time()
+
+def evaluate(model, eval_data, bptt, criterion, ntokens, device):
+    model.eval()  # turn on evaluation mode
+    total_loss = 0.
+    src_mask = generate_square_subsequent_mask(bptt).to(device)
+    with torch.no_grad():
+        for i in range(0, eval_data.size(0) - 1, bptt):
+            data, targets = get_batch(eval_data, i)
+            batch_size = data.size(0)
+            if batch_size != bptt:
+                src_mask = src_mask[:batch_size, :batch_size]
+            output = model(data, src_mask)
+            output_flat = output.view(-1, ntokens)
+            total_loss += batch_size * criterion(output_flat, targets).item()
+    return total_loss / (len(eval_data) - 1)
+    
+best_val_loss = float('inf')
+epochs = 120
+
+for epoch in range(1, epochs + 1):
+    epoch_start_time = time.time()
+    train(model, train_data, bptt, criterion, ntokens, device)
+    val_loss = evaluate(model, val_data, bptt, criterion, ntokens, device)
+    val_ppl = math.exp(val_loss)
+    elapsed = time.time() - epoch_start_time
+    print('-' * 89)
+    print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+          f'valid loss {val_loss:5.2f} | valid ppl {val_ppl:8.2f}')
+    print('-' * 89)
+
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+
+        torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                }, 'exps/lm/saved/d_%s.pth'%(epoch))

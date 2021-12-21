1d9d6ac5912f94edcb228a56fcbe510d343c6338
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2021-12-21 10:14:01 +0700

    Create _utils.py

---
+++ exps/conv/_utils.py
@@ -0,0 +1,86 @@
+import torch
+from tqdm.auto import tqdm
+
+def calculate_topk_accuracy(y_pred, y, k = 5):
+    with torch.no_grad():
+        batch_size = y.shape[0]
+        _, top_pred = y_pred.topk(k, 1)
+        top_pred = top_pred.t()
+        correct = top_pred.eq(y.view(1, -1).expand_as(top_pred))
+        correct_1 = correct[:1].reshape(-1).float().sum(0, keepdim = True)
+        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim = True)
+        acc_1 = correct_1 / batch_size
+        acc_k = correct_k / batch_size
+    return acc_1, acc_k
+
+def train(model, iterator, optimizer, criterion, device):
+    
+    epoch_loss = 0
+    epoch_acc_1 = 0
+    epoch_acc_5 = 0
+    
+    model.train()
+    
+    for (x, y) in tqdm(iterator, total=len(iterator)):
+        
+        x = x.to(device)
+        y = y.to(device)
+        
+        optimizer.zero_grad()
+                
+        y_pred = model(x)
+        
+        loss = criterion(y_pred, y)
+        
+        acc_1, acc_5 = calculate_topk_accuracy(y_pred, y)
+        
+        loss.backward()
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc_1 += acc_1.item()
+        epoch_acc_5 += acc_5.item()
+        
+    epoch_loss /= len(iterator)
+    epoch_acc_1 /= len(iterator)
+    epoch_acc_5 /= len(iterator)
+        
+    return epoch_loss, epoch_acc_1, epoch_acc_5
+
+def evaluate(model, iterator, criterion, device):
+    
+    epoch_loss = 0
+    epoch_acc_1 = 0
+    epoch_acc_5 = 0
+    
+    model.eval()
+    
+    with torch.no_grad():
+        
+        for (x, y) in tqdm(iterator, total=len(iterator)):
+
+            x = x.to(device)
+            y = y.to(device)
+
+            y_pred = model(x)
+
+            loss = criterion(y_pred, y)
+
+            acc_1, acc_5 = calculate_topk_accuracy(y_pred, y)
+
+            epoch_loss += loss.item()
+            epoch_acc_1 += acc_1.item()
+            epoch_acc_5 += acc_5.item()
+        
+    epoch_loss /= len(iterator)
+    epoch_acc_1 /= len(iterator)
+    epoch_acc_5 /= len(iterator)
+        
+    return epoch_loss, epoch_acc_1, epoch_acc_5
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs

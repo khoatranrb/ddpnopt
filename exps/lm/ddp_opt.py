15f47a38d06a24a4bb4dabd2be07024faf7c0928
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2022-02-23 11:20:39 +0700

    Update ddp_opt.py

--- exps/lm/ddp_opt.py
+++ exps/lm/ddp_opt.py
@@ -3,33 +3,19 @@ from torch.optim.optimizer import Optimizer
 from torch import Tensor
 from typing import List, Optional
 
-def update(params: List[Tensor],
-            grads: List[Tensor],
-            square_avgs: List[Tensor],
-            *,
-            lr: float,
-            alpha: float,
-            eps: float):
-
-    for i, param in enumerate(params):
-        grad = grads[i]
-        square_avg = square_avgs[i]
-
-        square_avg.mul_(alpha).addcmul_(grad, grad, value=1 - alpha)
-
-        avg = square_avg.sqrt().add_(eps)
-
-        param.addcdiv_(grad, avg, value=-lr)
-
-
+                        
 class DDPNOPT(Optimizer):
     def __init__(self, model, lr=1e-2, lrddp = 1e-2, alpha=0.99, eps=1e-8, weight_decay=0, momentum=0, centered=False):
         self.lr = lr
         self.lrddp = lrddp
         self.model = model
         self.eps = eps
+        self.alpha = alpha
+        self.first_time = True
         defaults = dict(lr=lr, momentum=momentum, alpha=alpha, eps=eps, centered=centered, weight_decay=weight_decay)
         super(DDPNOPT, self).__init__(model.parameters(), defaults)
+        for p in self.model.parameters():
+            self.state[p]['feedback'] = False
 
     def __setstate__(self, state):
         super(DDPNOPT, self).__setstate__(state)
@@ -39,34 +25,30 @@ class DDPNOPT(Optimizer):
 
     @torch.no_grad()
     def step(self, src, mask):
+        if self.first_time:
+            self.model(src, mask, update=True, opt=self, self.first_time)
+            self.first_time = False
         for group in self.param_groups:
-            params_with_grad = []
-            grads = []
-            square_avgs = []
-            grad_avgs = []
+            for p in group['params']:
+                if p.grad is None or self.state[p]['feedback']:
+                    continue
+                state = self.state[p]
+                if len(state) == 0:
+                    state['square_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                state['square_avg'].mul_(self.alpha).addcmul_(p.grad, p.grad, value=1 - self.alpha)
+                avg = state['square_avg'].sqrt().add_(self.eps)
+#                 state['hess'] = avg
+                p.addcdiv_(p.grad, avg, value=-self.lr)
+#         self.update()
+        if self.lrddp: self.model(src, mask, update=True, opt=self)
 
+    def update(self):
+        for group in self.param_groups:
             for p in group['params']:
                 if p.grad is None:
                     continue
-                params_with_grad.append(p)
-                grads.append(p.grad)
-
                 state = self.state[p]
-
-                # State initialization
-                if len(state) == 0:
-                    state['step'] = 0
-                    state['square_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
-
-                square_avgs.append(state['square_avg'])
-
-                state['step'] += 1
-
-
-        update(params_with_grad,
-                  grads,
-                  square_avgs,
-                  lr=self.lr,
-                  alpha=group['alpha'],
-                  eps=group['eps'])
-        if self.lrddp: self.model(src, mask, update=True, opt=self)
+                state['square_avg'].mul_(self.alpha).addcmul_(p.grad, p.grad, value=1 - self.alpha)
+                avg = state['square_avg'].sqrt().add_(self.eps)
+                state['hess'] = avg
+                p.addcdiv_(p.grad, avg, value=-self.lr)

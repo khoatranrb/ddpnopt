d525452dd5ccca422a27d05c42cee58ba0dcc2b5
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2022-02-24 17:40:45 +0700

    Update ddpnopt.py

--- ddpnopt.py
+++ ddpnopt.py
@@ -31,23 +31,27 @@ class Step(nn.Module):
 
     @torch.no_grad()
     def update(self, inp, opt):
+        if opt.first_time:
+            opt.state[self.mod.weight]['feedback'] = True
+            return self.forward_wo_train(inp)
         Q_u = self.mod.weight.grad
-        Q_uu = opt.state[self.mod.weight]['hess']
         Q_x = self.x.grad.mean(dim=0)
         if self.type=='Linear':
             Q_ux = calc_q_ux_fc(Q_u, Q_x.unsqueeze(-1))
-            big_k = calc_big_k_fc(Q_uu, Q_ux)
+#             big_k = calc_big_k_fc(Q_uu, Q_ux)
+            big_k = -Q_ux
             term2 = torch.einsum('xy,zt->xt', big_k, (inp - self.x_old).mean(dim=0).unsqueeze(-1)).squeeze(-1).reshape(
                 self.mod.weight.shape)
         else:
             Q_ux = calc_q_ux_conv(self.mod.weight, Q_u)
-            big_k = calc_big_k_conv(Q_uu, Q_ux)
+#             big_k = calc_big_k_conv(Q_uu, Q_ux)
+            big_k = -Q_ux
             term2 = calc_term2_conv(big_k, inp - self.x_old).reshape(self.mod.weight.shape)
-        self.mod.weight += opt.lr*opt.lrddp*(term2)
+        grad = self.mod.weight.grad - opt.lrddp*(term2)
+        opt.update([self.mod.weight], [grad])
         out = self.forward_wo_train(inp)
         del self.x
         del self.x_old
-        del opt.state[self.mod.weight]['hess']
         return out
 
 
@@ -74,7 +78,7 @@ def calc_term2_conv(big_k, delta_x):
     return out
 
 
-class DDPNOPT(Optimizer):
+class RmsDDP(Optimizer):
     def __init__(self, model, lr=1e-2, lrddp = 1e-2, alpha=0.99, eps=1e-8, weight_decay=0, momentum=0, centered=False):
         self.lr = lr
         self.lrddp = lrddp
@@ -82,40 +86,39 @@ class DDPNOPT(Optimizer):
         self.eps = eps
         self.alpha = alpha
         defaults = dict(lr=lr, momentum=momentum, alpha=alpha, eps=eps, centered=centered, weight_decay=weight_decay)
-        super(DDPNOPT, self).__init__(model.parameters(), defaults)
+        super(RmsDDP, self).__init__(model.parameters(), defaults)
+        self.first_time = True
+        for p in self.model.parameters():
+            self.state[p]['feedback'] = False
+            self.state[p]['square_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
 
     def __setstate__(self, state):
-        super(DDPNOPT, self).__setstate__(state)
+        super(RmsDDP, self).__setstate__(state)
         for group in self.param_groups:
             group.setdefault('momentum', 0)
             group.setdefault('centered', False)
 
     @torch.no_grad()
     def step(self):
+        if self.first_time and self.lrddp != 0::
+            self.model(src, mask, update=True, opt=self)
+            self.first_time = False
+        list_params, list_grad = [], []
         for group in self.param_groups:
             for p in group['params']:
-                if p.grad is None:
+                if p.grad is None or self.state[p]['feedback']:
                     continue
-                state = self.state[p]
-                if len(state) == 0:
-                    state['step'] = 0
-                    state['square_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
-                state['step'] += 1
-
-
-        self.update()
+                list_params.append(p)
+                list_grad.append(p.grad)
+        self.update(list_params, list_grad)
         if self.lrddp: self.model(update=True, opt=self)
 
-    def update(self):
-        for group in self.param_groups:
-            for p in group['params']:
-                if p.grad is None:
-                    continue
-                state = self.state[p]
-                state['square_avg'].mul_(self.alpha).addcmul_(p.grad, p.grad, value=1 - self.alpha)
-                avg = state['square_avg'].sqrt().add_(self.eps)
-                state['hess'] = avg
-                p.addcdiv_(p.grad, avg, value=-self.lr)
+    def update(self, list_params, list_grad):
+        for p, grad in zip(list_params, list_grad):
+            state = self.state[p]
+            state['square_avg'].mul_(self.alpha).addcmul_(grad, grad, value=1 - self.alpha)
+            avg = state['square_avg'].sqrt().add_(self.eps)
+            p.addcdiv_(grad, avg, value=-self.lr)
                 
 class AdamDDP(Optimizer):
     def __init__(self, model, lr=1e-3, lrddp = 1e-2, betas=(0.9, 0.999), eps=1e-8,
@@ -139,6 +142,12 @@ class AdamDDP(Optimizer):
         self.eps = eps
         self.beta1 = betas[0]
         self.beta2 = betas[1]
+        self.first_time = True
+        for p in self.model.parameters():
+            self.state[p]['feedback'] = False
+            self.state[p]['step'] = 0
+            self.state[p]['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+            self.state[p]['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
 
     def __setstate__(self, state):
         super(AdamDDP, self).__setstate__(state)
@@ -147,39 +156,33 @@ class AdamDDP(Optimizer):
 
     @torch.no_grad()
     def step(self):
+        if self.first_time and self.lrddp != 0:
+            self.model(update=True, opt=self)
+            self.first_time = False
+        list_params, list_grad = [], []
         for group in self.param_groups:
             for p in group['params']:
-                if p.grad is not None:
+                if p.grad is not None and not self.state[p]['feedback']:
                     if p.grad.is_sparse:
                         raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
-
-                    state = self.state[p]
-                    # Lazy state initialization
-                    if len(state) == 0:
-                        state['step'] = 0
-                        state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
-                        state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
-
-                    state['step'] += 1
-
-            self.update()
+        
+                    list_params.append(p)
+                    list_grad.append(p.grad)
+            self.update(list_params, list_grad)
             if self.lrddp: self.model(update=True, opt=self)
 
-    def update(self):
-      for group in self.param_groups:
-          for p in group['params']:
-              if p.grad is not None:
-                  if p.grad.is_sparse:
-                      raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
-                  state = self.state[p]
-                  bias_correction1 = 1 - self.beta1 ** state['step']
-                  bias_correction2 = 1 - self.beta2 ** state['step']
-                  
-                  state['exp_avg'].mul_(self.beta1).add_(p.grad, alpha=1 - self.beta1)
-                  state['exp_avg_sq'].mul_(self.beta2).addcmul_(p.grad, p.grad.conj(), value=1 - self.beta2)
-                  
-                  denom = (state['exp_avg_sq'].sqrt() / math.sqrt(bias_correction2)).add_(self.eps)
-                  state['hess'] = denom
-                  step_size = self.lr / bias_correction1
-                  
-                  p.addcdiv_(state['exp_avg'], denom, value=-step_size)
+    def update(self, list_params, list_grad):
+        for p, grad in zip(list_params, list_grad):
+              state = self.state[p]
+              state['step'] += 1
+              bias_correction1 = 1 - self.beta1 ** state['step']
+              bias_correction2 = 1 - self.beta2 ** state['step']
+
+              state['exp_avg'].mul_(self.beta1).add_(grad, alpha=1 - self.beta1)
+              state['exp_avg_sq'].mul_(self.beta2).addcmul_(grad, grad.conj(), value=1 - self.beta2)
+
+              denom = (state['exp_avg_sq'].sqrt() / math.sqrt(bias_correction2)).add_(self.eps)
+              step_size = self.lr / bias_correction1
+
+              p.addcdiv_(state['exp_avg'], denom, value=-step_size)
+                
